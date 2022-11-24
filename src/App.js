c0ef8346dc032c0cb1e2a417f23866ac2179bfbc
FatmirKondi <85420263+FatmirKondi@users.noreply.github.com>
2022-11-24 14:52:39 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDoList from './components/ToDoList';
+
+function App() {
+  return (
+    <div className="ToDoList">
+    <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
